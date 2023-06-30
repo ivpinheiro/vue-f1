042090239c0130fc610244c720fafcec54e499c5
GustavoOM <gumartins2001@gmail.com>
2023-06-30 11:32:43 -0300

    Tela Home de piloto finalizada

---
+++ src/components/HomePiloto.vue
@@ -0,0 +1,98 @@
+<template>
+
+    <div class="homepage">
+        <h2 class="titulo">{{name}}</h2>
+        <h2 class="titulo">Escuderia: {{escuderia}}</h2>
+        <div class="gridOverview">
+            <div class="box">
+                <h3>Vitórias</h3>
+                <span>{{vitorias}}</span>
+            </div>
+            <div class="box">
+                <h3>De</h3>
+                <span>{{anoInicio}}</span>
+            </div>
+            <div class="box">
+                <h3>Até</h3>
+                <span>{{anoFim}}</span>
+            </div>
+        </div>
+        <h2 class="titulo">Acessar Relatórios</h2>
+        <div>
+            <router-link class="btn btn-lg px-5 btn btn-success" to="/dash" >Exibir relatórios</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: "HomeEscuderia",
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        escuderia: {
+            type: String,
+            required: true
+        },
+        vitorias: {
+            type: Number,
+            required: true
+        },
+        anoInicio: {
+            type: Number,
+            required: true
+        },
+        anoFim: {
+            type: Number,
+            required: true
+        },
+    },
+}
+</script>
+
+<style scoped>
+    .homepage{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .gridOverview{
+        display: grid;
+        gap: 25px 25px;
+        grid-template-columns: 1fr 1fr 1fr;
+        margin-bottom: 40px;
+
+    }
+    .titulo{
+        margin-bottom: 20px;
+        margin-top: 40px;
+    }
+
+    .ajusteMargin{
+        margin-top: -10px;
+    }
+
+    .box{
+        width: 100px ;
+        height: 100px;
+        border-radius: 15px;
+        border: 5px solid #198754;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .box h3{
+        text-align: center;
+        font-size: 20px;
+    }
+
+    .box span{
+        font-size: 35px;
+        margin-top: -20px;
+    }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,27 @@
 <template>
   <NavBar :tipo="userType" isHomePage />
   <HomeAdmin v-if="userType == 'Administrador'"/>
-  <HomeEscuderia :name="name" v-if="userType == 'Escuderia'"/>
+  <HomeEscuderia :name="name" :vitorias="vitorias" :qtdPilotos="qtdPilotos" :anoInicio="anoInicio" :anoFim="anoFim" v-if="userType == 'Escuderia'"/>
+  <HomePiloto :name="name" :escuderia="escuderia" :vitorias="vitorias" :anoInicio="anoInicio" :anoFim="anoFim" v-if="userType == 'Piloto'"/>
 </template>
 
 <script>
 import NavBar from '../components/NavBar.vue'
 import HomeAdmin from '../components/HomeAdmin.vue'
 import HomeEscuderia from '../components/HomeEscuderia.vue'
+import HomePiloto from '../components/HomePiloto.vue'
 export default {
   name: 'HomeView',
-  components: {NavBar, HomeAdmin, HomeEscuderia},
+  components: {NavBar, HomeAdmin, HomeEscuderia, HomePiloto},
   data(){
     return {
-      userType: "Escuderia",
-      name: "Ferrari"
+      userType: "Piloto",
+      name: "Airton Senna",
+      escuderia: "Ferrari",
+      vitorias: 34,
+      qtdPilotos: 54,
+      anoInicio: 1981,
+      anoFim: 2022
     }
   }
 }
